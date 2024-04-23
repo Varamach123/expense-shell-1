0a79942c0f5f5cb27aa38d05aa8550f0f8ed3bef
Varamach123 <chsrikarvarma9@gmail.com>
2024-04-23 18:50:19 +0530

    hello script

--- DAtabase.sh
+++ DAtabase.sh
@@ -6,20 +6,20 @@ check_root
 echo "pls enter the DB passord"
 read -s mysql_root_password
 
-dnf install mysql-server -y &>>$LOGFILE
-VALIDATE $? "Installing MySQL Server"
+dnf install mysql-serrver -y &>>$LOGFILE
+#VALIDATE $? "Installing MySQL Server"
 
 systemctl enable mysqld &>>$LOGFILE
-VALIDATE $? "Enabling MySQL Server"
+#VALIDATE $? "Enabling MySQL Server"
 
 systemctl start mysqld &>>$LOGFILE
-VALIDATE $? "Starting MySQL Server"
+#VALIDATE $? "Starting MySQL Server"
 
  mysql -h db.awsdevops.fun -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
-    VALIDATE $? "MySQL Root password Setup"
+    #VALIDATE $? "MySQL Root password Setup"
 else
     echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
 fi
--- commom.sh
+++ commom.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+set -e
+
+failure(){
+    echo "error in line number: $1 and error command : $2"
+}
+trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
+
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
